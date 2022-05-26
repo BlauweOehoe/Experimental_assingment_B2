99b91dbb023e686a72f0352b06435d97367c4178
FemianD <98589496+FemianD@users.noreply.github.com>
2022-05-26 14:09:54 +0200

    Add files via upload

--- B2_idea 3/Analyse.R
+++ B2_idea 3/Analyse.R
@@ -1,6 +1,8 @@
 library(haven)
 library(dplyr)
 library(tidyr)
+library(effectsize)
+library(emmeans)
 
 #Load data
 df <- read_sav("Exp+Res+2022+Idea+3+Manipulate+&+loyalty_May+9,+2022_14.59.sav")
@@ -13,8 +15,8 @@ df <- df %>%
 #Add treatment variable + means loyalty and NFU
 df_analyse <- df %>%
   mutate(treatment = case_when(loyalty_DO_higher == 1 ~ 1, loyalty_DO_control == 1 ~ 0)) %>%
-  mutate(loyalty_mean = rowMeans(select(df_analyse, loyalty_1:loyalty_2))) %>%
-  mutate(NFU_mean = rowMeans(select(df_analyse, NFU_similarity_2:NFU_similarity_4)))
+  mutate(loyalty_mean = rowMeans(select(df, loyalty_1:loyalty_2))) %>%
+  mutate(NFU_mean = rowMeans(select(df, NFU_similarity_2:NFU_similarity_4)))
 
 #Effect of rank and NFU on loyalty
 regression_1 <- lm(loyalty_mean ~ NFU_mean * relative_rank_1 + treatment, data = df_analyse) 
@@ -28,3 +30,18 @@ summary(regression_2)
 regression_3 <- lm(loyalty_mean ~ NFU_mean * relative_rank_1 + product_choice, data = df_analyse)
 summary(regression_3)
 
+#Anova
+anova_1 <- aov(loyalty_mean ~ NFU_mean * relative_rank_1 + treatment, data = df_analyse)
+summary(anova_1)
+
+anova_2 <- aov(loyalty_mean ~ NFU_mean * relative_rank_1 + product_choice, data = df_analyse)
+
+#Eta squared
+eta_squared(anova_1)
+
+#Differences in effect
+post_hoc <- emmeans(anova_1, pairwise ~ treatment, adjust="bonferroni")
+summary(post_hoc)
+
+post_hoc_product <- emmeans(anova_2, pairwise ~ product_choice, adjust="bonferroni")
+summary(post_hoc_product)
