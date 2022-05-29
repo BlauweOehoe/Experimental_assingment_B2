465c9ad103b5dc6e74b70a4443a3d8eee2918b15
BlauweOehoe <m.lammertink@tilburguniversity.edu>
2022-05-29 16:50:49 +0200

    added and cleaned code

--- B2_idea 2/code_idea2.R
+++ B2_idea 2/code_idea2.R
@@ -14,15 +14,12 @@ library(broom)
 #loading survey results into R
 survey_df<-read_sav("Exp+Res+2022+Idea+2+-+premium+vs+private+label_May+9,+2022_15.03.sav")
 
-#cleaning the data, Private label is 1, Premium = 0
-#survey_clean<-survey_df %>% 
-#  mutate(private_v_premium = replace_na(Main_Block_DO_buy_private, 0)) %>% 
-#  mutate(mean_wtp = (WTP_self + WTP_others)/2)
-
+#cleaning the data & Removing outliers, Private label is 1, Premium = 0
 survey_clean<-survey_df %>% 
   mutate(private_v_premium = replace_na(Main_Block_DO_buy_private, 0)) %>% 
   mutate(mean_wtp = (WTP_others - WTP_self)) %>% 
-  filter(mean_wtp >= -5)
+  filter(mean_wtp >= -1) %>% 
+  filter(mean_wtp <= 1)
 
   
 #keeping only relevant data
@@ -32,7 +29,9 @@ survey_tidy<-survey_clean %>%
          relative_rank_1,
          mean_wtp,
          Age,
-         gender
+         gender,
+         WTP_others,
+         WTP_self
          )
 
 #descriptive statistics
@@ -40,42 +39,31 @@ descriptive_stat <- survey_tidy %>%
   group_by(private_v_premium) %>%
   summarize_all(mean, na.rm = TRUE)
 
+##H1
+#effect of X on Mediator
+regr_xm<-lm(relative_rank_1 ~ private_v_premium, survey_tidy)
+summary(regr_xm)
+
+#effect of X on Mediator adding age
+regr_xma<-lm(relative_rank_1 ~ private_v_premium + Age, survey_tidy)
+summary(regr_xm)
+
+##H2
+#effect of Mediator on Y
+regr_my<-lm(mean_wtp ~ relative_rank_1, survey_tidy)
+summary(regr_my)
+
+#total model 
+regr_full<- lm(mean_wtp ~ private_v_premium + relative_rank_1 + Age, survey_tidy)
+summary(regr_full)
+
 ##Testing assumptions
 
 #Normality
 ggplot(survey_tidy, aes(mean_wtp))+geom_histogram(bins = 50)
 shapiro.test(survey_tidy$mean_wtp)
 
-#effect of X on Mediator
-regr_xm<-lm(relative_rank_1 ~ private_v_premium, survey_tidy)
-summary(regr_xm)
-
-# effect of X on Mediator adding Age
-regr_xm1<-lm(relative_rank_1 ~ private_v_premium + Age, survey_tidy)
-summary(regr_xm1)
-
-#effect of Mediator on Y
-regr_my<-lm(mean_wtp ~ relative_rank_1, survey_tidy)
-summary(regr_my)
-
-#total effect
-regr_all<-lm(mean_wtp ~ private_v_premium, survey_tidy)
-summary(regr_all)
-
-
-#effect of X+M+ M*X on Y
-regr_total<- lm(mean_wtp ~ private_v_premium + relative_rank_1 + Age + private_v_premium*relative_rank_1 , survey_tidy)
-summary(regr_total)
-
-#effect of M*X on Y
-regr_med<- lm(mean_wtp ~ private_v_premium * relative_rank_1, survey_tidy)
-summary(regr_med)
-
-#total model 
-regr_full<- lm(mean_wtp ~ private_v_premium + relative_rank_1, survey_tidy)
-summary(regr_full)
-
-#Evaluate model assumptions of the regression
+#Evaluate model assumptions of the Full_regr
 autoplot(
   regr_full,
   which = 1:3,
@@ -83,6 +71,31 @@ autoplot(
   ncol = 3
 )
 
+#Evaluate model assumptions of the H1 x-->m,
+autoplot(
+  regr_xm,
+  which = 1:3,
+  nrow = 1,
+  ncol = 3
+)
+
+#evaluate model of X on M adding age
+autoplot(
+  regr_xma,
+  which = 1:3,
+  nrow = 1,
+  ncol = 3
+)
+
+#Evaluate model assumptions of the H2
+autoplot(
+  regr_my,
+  which = 1:3,
+  nrow = 1,
+  ncol = 3
+)
+
+##ANOVA & Post-Hoc
 #Anova treatment
 anova_1 <- aov(mean_wtp ~ private_v_premium + relative_rank_1 + Age, survey_tidy)
 summary(anova_1)
